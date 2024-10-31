48ccc31c372ba5c8a20195c244e9e4ad7c49f7bc
edgar vitor <vitoredgar888@gmail.com>
2024-10-31 18:50:16 -0300

    adicionando modo noturno

--- script.js
+++ script.js
@@ -34,3 +34,37 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+
+// modo noturno 
+
+function noturnoModo(){
+
+    document.body.style.background = "#011d40";
+    document.querySelector(".contanier-login").style.background = "#1d1d1d";
+    document.querySelector(".contanier-login").style.border = "#cccccc solid 2px";
+    let paragrafo = document.querySelectorAll("p")
+
+    for(let i = 0 ; i < paragrafo.length ; i++){
+        paragrafo[i].style.color = '#6b6b6b';
+    }
+
+    document.querySelector("h1").style.color = '#ffffff';
+    document.querySelector("h3").style.color = '#ffffff';
+    document.querySelector("h2").style.color = '#e4dbfb';
+    document.querySelector(".noturno").style.display = 'none';
+
+    let alterarCorBotao = document.querySelectorAll('button')
+    let alterarBotao = document.querySelectorAll('button')
+
+    for(let x = 0 ; x < alterarCorBotao.length ; x++){
+
+        alterarCorBotao[x].style.background = '#011d40';
+
+    }
+
+    for(i = 0 ; i < alterarBotao.length ; i++){
+        alterarBotao[i].style.border = "#cccccc solid 1px";
+
+    }
+
+}
